492ee8366327d2f4477a67dd064430299e28abd1
kaherro <torkomyanvv@gmail.com>
2026-01-16 19:43:01 +0300

    [feat]: Moved base62 converting function to base62.cpp (redesigned project)

--- main.cpp
+++ main.cpp
@@ -1,33 +1,12 @@
 #include "crow_all.h"
 #include "database.h"
+#include "base62.h"
 #include <iostream>
 #include <algorithm>
 #include <chrono>
 #include <format> 
 #include <string>
 #include <unordered_map>
-
-std::unordered_map<int, char> table_10_to_62 = {
-    {0, '0'}, {1, '1'}, {2, '2'}, {3, '3'}, {4, '4'}, {5, '5'}, {6, '6'}, {7, '7'}, {8, '8'}, {9, '9'},
-    {10, 'A'}, {11, 'B'}, {12, 'C'}, {13, 'D'}, {14, 'E'}, {15, 'F'}, {16, 'G'}, {17, 'H'}, {18, 'I'}, {19, 'J'}, 
-    {20, 'K'}, {21, 'L'}, {22, 'M'}, {23, 'N'}, {24, 'O'}, {25, 'P'}, {26, 'Q'}, {27, 'R'}, {28, 'S'}, {29, 'T'}, 
-    {30, 'U'}, {31, 'V'}, {32, 'W'}, {33, 'X'}, {34, 'Y'}, {35, 'Z'}, 
-    {36, 'a'}, {37, 'b'}, {38, 'c'}, {39, 'd'}, {40, 'e'}, {41, 'f'}, {42, 'g'}, {43, 'h'}, {44, 'i'}, {45, 'j'}, 
-    {46, 'k'}, {47, 'l'}, {48, 'm'}, {49, 'n'}, {50, 'o'}, {51, 'p'}, {52, 'q'}, {53, 'r'}, {54, 's'}, {55, 't'}, 
-    {56, 'u'}, {57, 'v'}, {58, 'w'}, {59, 'x'}, {60, 'y'}, {61, 'z'} 
-}; 
-
-std::string to_base62(int id10) {
-    std::string id62 = "";
-    while(id10 > 62) {
-        id62 += table_10_to_62[id10 % 62]; 
-        id10 /= 62; 
-    } 
-    if(id10) id62 += table_10_to_62[id10]; 
-    reverse(id62.begin(), id62.end()); 
-    return id62; 
-}
-
 
 crow::response redirect(const std::string& url)
 {
@@ -85,7 +64,6 @@ int main()
         
         return redirect(original_url); 
     });
-
 
     app.port(18080).multithreaded().run(); 
 }
