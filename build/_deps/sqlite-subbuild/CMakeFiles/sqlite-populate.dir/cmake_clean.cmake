a85360bbd4d4952e98d27e27c6abb9a55ed91ca5
kaherro <torkomyanvv@gmail.com>
2026-01-17 21:02:50 +0300

    [fix]: Deleted asio importing in CMakeLists.txt

---
+++ build/_deps/sqlite-subbuild/CMakeFiles/sqlite-populate.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sqlite-populate"
+  "CMakeFiles/sqlite-populate-complete"
+  "sqlite-populate-prefix/src/sqlite-populate-stamp/sqlite-populate-build"
+  "sqlite-populate-prefix/src/sqlite-populate-stamp/sqlite-populate-configure"
+  "sqlite-populate-prefix/src/sqlite-populate-stamp/sqlite-populate-download"
+  "sqlite-populate-prefix/src/sqlite-populate-stamp/sqlite-populate-install"
+  "sqlite-populate-prefix/src/sqlite-populate-stamp/sqlite-populate-mkdir"
+  "sqlite-populate-prefix/src/sqlite-populate-stamp/sqlite-populate-patch"
+  "sqlite-populate-prefix/src/sqlite-populate-stamp/sqlite-populate-test"
+  "sqlite-populate-prefix/src/sqlite-populate-stamp/sqlite-populate-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/sqlite-populate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
